6bab1f0baefba78b927276925c809478f0a38931
Erikas Staugas <erikstaugas1@gmail.com>
2022-10-01 18:38:18 +0100

    Find frequency of elements

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,13 @@
 """Frequencies function."""
-"""ENTER YOUR SOLUTION HERE!"""
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    # Converts entire list into string
+    str_items = [str(i) for i in items]
+    
+    for i in str_items:
+        count = str_items.count(i) # Count up all elements
+        frequencies[i] = count
     return frequencies
+
+print(frequencies([100, 'Hello', '100', '100', 100]))
